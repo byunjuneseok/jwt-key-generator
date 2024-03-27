165461de936ebe52fb06c61c3bd7b875eaf88525
byunjuneseok <byunjuneseok@gmail.com>
2024-03-27 20:31:08 +0900

    refactor: 불필요한 코드 정리

--- src/main/kotlin/com/binaryflavor/jwtkeygenerator/Main.kt
+++ src/main/kotlin/com/binaryflavor/jwtkeygenerator/Main.kt
@@ -4,7 +4,6 @@ import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
 
-// get arguments
 fun main(args: Array<String>) {
     val parser = ArgParser("JwtKeyGenerator")
     val algorithm by parser.option(
@@ -12,24 +11,24 @@ fun main(args: Array<String>) {
         shortName = "a",
         description = "Format for output file",
     ).default(Algorithms.HS256)
-    val generator = JwtKeyGenerator()
     parser.parse(args)
-    println(algorithm)
-    when (algorithm) {
-        Algorithms.HS256 -> {
-            println(generator.generateKeyForHS256())
-        }
-        Algorithms.HS384 -> {
-            println(generator.generateKeyForHS384())
-        }
-        Algorithms.HS512 -> {
-            println(generator.generateKeyForHS512())
-        }
-        Algorithms.RS256 -> {
-            println(generator.generateKeysForRS256())
-        }
-        Algorithms.RS512 -> {
-            println(generator.generateKeysForRS512())
+    JwtKeyGenerator().let {
+        when (algorithm) {
+            Algorithms.HS256 -> {
+                println(it.generateKeyForHS256())
+            }
+            Algorithms.HS384 -> {
+                println(it.generateKeyForHS384())
+            }
+            Algorithms.HS512 -> {
+                println(it.generateKeyForHS512())
+            }
+            Algorithms.RS256 -> {
+                println(it.generateKeysForRS256())
+            }
+            Algorithms.RS512 -> {
+                println(it.generateKeysForRS512())
+            }
         }
     }
 }
